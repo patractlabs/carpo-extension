1498c6a8cd7951bcd2a758582aa7c39aae95bc20
Zhichao Zhang <zzcwoshizz@163.com>
2021-09-07 16:49:46 +0800

    test cli

--- packages/test/view/Test.tsx
+++ packages/test/view/Test.tsx
@@ -1,16 +1,41 @@
 import type { Uri } from '@carpo/common/types';
 
+import { sendMessage } from '@carpo/common/sendMessage';
 import { RowItem, useRedspot } from '@carpo/react-components';
 import { Button, Select, Switch } from 'antd';
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const Test: React.FC = () => {
   const { workspacePath } = useRedspot();
 
   const [noCompile, setNoCompile] = useState(false);
-  const [testFiles] = useState<Uri[]>([]);
+  const [testFiles, setTestFiles] = useState<Uri[]>([]);
   const [selectFile, setSelectFile] = useState<string>();
-  const [running, setRunning] = useState(false);
+  const [runningOne, setRunningOne] = useState(false);
+  const [runningAll, setRunningAll] = useState(false);
+
+  useEffect(() => {
+    sendMessage('redspot.subTestFiles', null, setTestFiles).then(setTestFiles).catch(console.error);
+  }, []);
+
+  const testOne = useCallback(async (): Promise<void> => {
+    if (selectFile) {
+      setRunningOne(true);
+
+      await sendMessage('redspot.test', {
+        noCompile,
+        filePath: selectFile
+      }).finally(() => setRunningOne(false));
+    }
+  }, [noCompile, selectFile]);
+
+  const testAll = useCallback(async (): Promise<void> => {
+    setRunningAll(true);
+
+    await sendMessage('redspot.test', {
+      noCompile
+    }).finally(() => setRunningAll(false));
+  }, [noCompile]);
 
   return (
     <>
@@ -27,11 +52,11 @@ const Test: React.FC = () => {
         <Switch checked={noCompile} onChange={setNoCompile} />
       </RowItem>
       <Button.Group style={{ marginTop: 8, width: '100%' }}>
-        <Button loading={running} style={{ width: '100%' }}>
-          {running ? 'Testing' : 'Test all'}
+        <Button loading={runningAll} onClick={testAll} style={{ width: '100%' }}>
+          {runningAll ? 'Testing' : 'Test all'}
         </Button>
-        <Button disabled={!selectFile} loading={running} style={{ width: '100%' }} type='primary'>
-          {running ? `${selectFile?.replace(workspacePath || '', '').replace(/^\//, '')} ...` : 'Test one'}
+        <Button disabled={!selectFile} loading={runningOne} onClick={testOne} style={{ width: '100%' }} type='primary'>
+          {runningOne ? `${selectFile?.replace(workspacePath || '', '').replace(/^\//, '')} ...` : 'Test one'}
         </Button>
       </Button.Group>
     </>
